d8a0ba1cec64b94c2ed3bf110b4c8ae1f2f76c8d
Kerlly Vizuete <kerlly.vizuete@outlook.com>
2024-11-01 05:56:28 -0500

    Una prueba unitaria para la prueba del primer bimestre
    Test  registrar administrador

--- src/test/java/com/example/service/RegistroSistemaServiceTest.java
+++ src/test/java/com/example/service/RegistroSistemaServiceTest.java
@@ -18,6 +18,7 @@ public class RegistroSistemaServiceTest {
 
         registroSistemaService = new RegistroSistemaService();
     }
+
     @Test
     void testRegistrarAlumno() {
         String nombre = "Kerlly";
@@ -29,8 +30,7 @@ public class RegistroSistemaServiceTest {
         Alumno alumno = alumnoDAO.findByEmail(correo);
         if (alumno == null) {
             registroSistemaService.registrarUsuario(nombre, apellido, correo, rolId, materiasSeleccionadas);
-        }
-        else {
+        } else {
             alumnoDAO.eliminarAlumno(String.valueOf(alumno.getId()));
             registroSistemaService.registrarUsuario(nombre, apellido, correo, rolId, materiasSeleccionadas);
 
@@ -50,6 +50,7 @@ public class RegistroSistemaServiceTest {
         alumnoDAO.eliminarAlumno(String.valueOf(alumnoTest.getId())); // Asegúrate de que el ID sea un String
         assertNull(alumnoDAO.findByEmail(correo), "El alumno debería haber sido eliminado.");
     }
+
     @Test
     void testRolInvalido() {
         String nombre = "Kerlly";
@@ -62,5 +63,39 @@ public class RegistroSistemaServiceTest {
             registroSistemaService.registrarUsuario(nombre, apellido, correo, rolId, materiasSeleccionadas);
         });
         assertEquals("Rol no válido", exception.getMessage());
+    }
+
+    @Test
+    void testRegistrarAdministrador() {
+        String nombre = "Kerlly";
+        String apellido = "Vizuete";
+        String correo = "ale.vizuete@epn.edu.ec";
+        String rolId = "3";
+        String[] materiasSeleccionadas = new String[0];
+
+        //Verificacion de datos de prueba
+        Administrador administrador = administradorDAO.findByEmail(correo);
+        if (administrador == null) {
+            registroSistemaService.registrarUsuario(nombre, apellido, correo, rolId, materiasSeleccionadas);
+        }
+        else {
+            administradorDAO.eliminarAdmin(String.valueOf(administrador.getId()));
+            registroSistemaService.registrarUsuario(nombre, apellido, correo, rolId, materiasSeleccionadas);
+
+        }
+        // Validar que el alumno fue registrado correctamente
+        Administrador administradorTest = administradorDAO.findByEmail(correo);
+        assertNotNull(administradorTest, "El administrador debería estar registrado.");
+        assertEquals(correo, administradorTest.getEmail(), "El correo del administrador debería coincidir.");
+
+        //Validar que no se puede crear un alumno con el mismo correo
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            registroSistemaService.registrarUsuario(nombre, apellido, correo, rolId, materiasSeleccionadas);
+        });
+        assertEquals("El correo ya está registrado: " + correo, exception.getMessage());
+
+        // Limpiar: eliminar el registro del alumno después de la prueba
+        alumnoDAO.eliminarAlumno(String.valueOf(administradorTest.getId())); // Asegúrate de que el ID sea un String
+        assertNull(alumnoDAO.findByEmail(correo), "El administrador debería haber sido eliminado.");
     }
 }
